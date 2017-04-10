25672e4c1fb2f2a196728bc863fb40cd521dbdc1
Brucol <1034727989@qq.com>
2017-04-10 19:23:06 +0800

    开始创建关键代码

---
+++ animal_identifier.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ASUS on 2017/4/10.
+ */
+function animal_identifier(r, f){
+    this.rules = r;
+    this.facts = f;
+
+    function Creat_Rules(){
+
+    }
+
+    function Creat_Fact(){
+
+    }
+
+    function reason(){
+
+    }
+}
